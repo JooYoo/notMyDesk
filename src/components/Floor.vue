ae6f5a9f09fb5e3c1a8b42e6ad397af535b7b225
JooYoo <jooyoo@outlook.com>
2023-01-13 12:15:24 +0100

    style: goup godown ui logic

--- src/components/Floor.vue
+++ src/components/Floor.vue
@@ -19,18 +19,59 @@ import WeeklyDatePickerComponent from "./WeeklyDatePicker.vue";
 import DeskGroupComponent from "./DeskGroup.vue";
 import RoomComponent from "./Room.vue";
 
+const floorGo = Object.freeze({
+  up: "up",
+  down: "down",
+});
+
 export default {
   components: {
     DeskGroup: DeskGroupComponent,
     WeeklyDatePicker: WeeklyDatePickerComponent,
     Room: RoomComponent,
   },
+  data() {
+    return {
+      direction: null,
+    };
+  },
   props: ["currentFloor"],
+  watch: {
+    currentFloor(newVal, oldVal) {
+      if (oldVal && newVal) {
+        this.isGoUp(oldVal.id, newVal.id);
+      }
+    },
+  },
   computed: {
     // prevent component rendering too fast
     isCurrentFloorExist() {
       return this.currentFloor;
     },
+    // TODO: if goup then add goup-css-class, otherwise add godown-css-class
+    directionClass() {
+      switch (this.direction) {
+        case floorGo.up:
+          return "goup-class";
+        case floorGo.down:
+          return "godown-class";
+        default:
+          return "";
+      }
+    },
+  },
+  methods: {
+    // watch switch floor state
+    isGoUp(oldId, newId) {
+      if (oldId < newId) {
+        this.direction = floorGo.up;
+      } else {
+        this.direction = floorGo.down;
+      }
+    },
+  },
+  updated() {
+    console.log(this.directionClass);
   },
 };
 </script>
@@ -46,15 +87,15 @@ export default {
   padding-top: unset;
   width: 100%;
   height: 100%;
+}
 
-  // TODO: wrap animation properties to new class
+.goup {
   animation-name: goup;
   animation-duration: 1s;
   animation-timing-function: linear;
   animation-fill-mode: forwards;
   animation-iteration-count: 1;
 }
-
 @keyframes goup {
   0% {
     transform: translate3d(0, 0, 0);
@@ -72,6 +113,14 @@ export default {
     transform: translate3d(0, 0%, 0);
     opacity: 1;
   }
+}
+
+.godown {
+  animation-name: godown;
+  animation-duration: 1s;
+  animation-timing-function: linear;
+  animation-fill-mode: forwards;
+  animation-iteration-count: 1;
 }
 
 @keyframes godown {
